ea2c45b9efce559398ee307947f557593e2af1a5
Daeho Choi <112150244+DaeHOHoHOHo@users.noreply.github.com>
2023-11-03 10:44:37 +0900

    Update check_ssh.py

--- serviceScanner/check_ssh.py
+++ serviceScanner/check_ssh.py
@@ -1,11 +1,15 @@
-import paramiko
+import socket
 
 def check_ssh(ip, port):
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh.connect(ip, port=port, timeout=2)
-        ssh.close()
-        return True
+        s = socket.create_connection((ip, port), timeout=2)
+        response = s.recv(1024)
+        s.close()
+
+        # SSH는 초기 응답으로 "SSH-"로 시작하는 문자열을 전송합니다.
+        if response.startswith(b"SSH-"):
+            return True
     except:
-        return False
+        pass
+
+    return False
