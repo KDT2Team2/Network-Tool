f445d2c556f8bc8cc01b72c91d9532e6170192a1
Daeho Choi <112150244+DaeHOHoHOHo@users.noreply.github.com>
2023-11-03 10:44:44 +0900

    Update check_ssh.py

--- serviceScanner/check_ssh.py
+++ serviceScanner/check_ssh.py
@@ -6,7 +6,6 @@ def check_ssh(ip, port):
         response = s.recv(1024)
         s.close()
 
-        # SSH는 초기 응답으로 "SSH-"로 시작하는 문자열을 전송합니다.
         if response.startswith(b"SSH-"):
             return True
     except:
